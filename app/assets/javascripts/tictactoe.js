48283378c064ec8cb6b39b6f355d2f420e22df5c
Adam Duff <j.adam.duff@gmail.com>
2018-11-15 08:47:28 +0000

    automatically backed up by learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -87,7 +87,6 @@ function saveGame() {
       type: 'PATCH',
       url: 'games/' + gameId,
       data: { "state": stateArr },
-      contentType: 'application/json',
       success: function(response) {
         console.log(response);
       }
