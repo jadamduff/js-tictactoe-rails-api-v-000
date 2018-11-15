b0a3d2cc0cd2b6764d1c2c9c6c0e1370120cb9fa
Adam Duff <j.adam.duff@gmail.com>
2018-11-15 06:15:27 +0000

    automatically backed up by learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,42 @@
-// Code your JavaScript / jQuery solution here
+let stateArr = ["", "", "", "", "", "", "", "", ""];
+
+const winningCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+
+let setTurn = function(arr) {
+  let counter = 0;
+  for (const space of arr) {
+    if (space !== "") {
+      counter ++;
+    }
+  }
+  return counter;
+}
+
+let turn = setTurn(stateArr);
+
+window.player = function() {
+  if (turn % 2 === 0) {
+    return "X";
+  } else {
+    return "O";
+  }
+}
+
+let findPosition = function(x_pos, y_pos) {
+  let x = parseInt(x_pos);
+  let y = parseInt(y_pos);
+  let arrPos = 0;
+  if (y === 1) {
+    arrPos = 3;
+  } else if (y === 2) {
+    arrPos = 6;
+  }
+  return arrPos += x;
+}
+
+$(document).ready(function() {
+
+  $('body').on('click', function() {
+    console.log('worked');
+  })
+});
