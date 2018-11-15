ac41a8fdbfdcdcb5a17acf8b7eb74de248a6dae5
Adam Duff <j.adam.duff@gmail.com>
2018-11-15 06:17:40 +0000

    automatically backed up by learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -2,7 +2,7 @@ let stateArr = ["", "", "", "", "", "", "", "", ""];
 
 const winningCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
-let setTurn = function(arr) {
+function setTurn(arr) {
   let counter = 0;
   for (const space of arr) {
     if (space !== "") {
