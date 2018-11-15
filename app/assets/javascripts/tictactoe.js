229e9ec2c92e917a7013b7e6321c495662fa5f22
Adam Duff <j.adam.duff@gmail.com>
2018-11-15 06:18:48 +0000

    automatically backed up by learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,4 +1,5 @@
 let stateArr = ["", "", "", "", "", "", "", "", ""];
+let turn = setTurn(stateArr);
 
 const winningCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
@@ -12,7 +13,7 @@ function setTurn(arr) {
   return counter;
 }
 
-let turn = setTurn(stateArr);
+
 
 function player() {
   if (turn % 2 === 0) {
