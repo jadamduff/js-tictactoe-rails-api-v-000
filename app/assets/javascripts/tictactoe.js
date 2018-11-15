480914db38c0668426ede0040afcb2d45f7ef4b1
Adam Duff <j.adam.duff@gmail.com>
2018-11-15 08:35:38 +0000

    automatically backed up by learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,5 +1,6 @@
 let stateArr = ["", "", "", "", "", "", "", "", ""];
 let turn = setTurn();
+let gameId = 'none';
 
 const winningCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
@@ -78,8 +79,8 @@ function clearBoard() {
 function saveGame() {
   let posting = $.post('/games', {"state": stateArr });
 
-  posting.done(function(data) {
-    console.log(data);
+  posting.done(function(response) {
+    console.log(response);
   });
 }
 
