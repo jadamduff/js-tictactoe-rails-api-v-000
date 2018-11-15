c01c6fff1774128d1d4b2f32cc34716cbff27cb7
Adam Duff <j.adam.duff@gmail.com>
2018-11-15 07:20:24 +0000

    automatically backed up by learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,5 +1,5 @@
 let stateArr = ["", "", "", "", "", "", "", "", ""];
-let turn = setTurn(stateArr);
+let turn = setTurn();
 
 const winningCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
@@ -41,7 +41,7 @@ function updateState(square) {
     $(td).text(stateArr[index]);
   });
 
-  turn = setTurn(stateArr);
+  turn = setTurn();
 }
 
 function checkWinner
